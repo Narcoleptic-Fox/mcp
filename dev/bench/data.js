e5e23fc680b1906add237013af40b8ce0a74097f
github-action-benchmark <github@users.noreply.github.com>
2025-05-16 21:08:19 +0000

    add Benchmark (go) benchmark result for ba2fae6fc0117008e188f011d9bcb19a0fc10069

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747409743474,
+  "lastUpdate": 1747429699794,
   "repoUrl": "https://github.com/Narcoleptic-Fox/mcp",
   "entries": {
     "Benchmark": [
@@ -94,6 +94,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkRequestSizes/Payload-10000KB - allocs/op",
             "value": 278,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bwclaus@gmail.com",
+            "name": "Brock Claussen",
+            "username": "Dieshen"
+          },
+          "committer": {
+            "email": "bwclaus@gmail.com",
+            "name": "Brock Claussen",
+            "username": "Dieshen"
+          },
+          "distinct": true,
+          "id": "ba2fae6fc0117008e188f011d9bcb19a0fc10069",
+          "message": "changed documentation file types: CHANGELOG, CODE_OF_CONDUCT, CONTRIBUTING, and README",
+          "timestamp": "2025-05-16T17:07:18-04:00",
+          "tree_id": "8261d98f875665e225835513b299f2fa715db365",
+          "url": "https://github.com/Narcoleptic-Fox/mcp/commit/ba2fae6fc0117008e188f011d9bcb19a0fc10069"
+        },
+        "date": 1747429698839,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkRequestSizes/Payload-10000KB",
+            "value": 2446115507,
+            "unit": "ns/op\t611695064 B/op\t     247 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRequestSizes/Payload-10000KB - ns/op",
+            "value": 2446115507,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRequestSizes/Payload-10000KB - B/op",
+            "value": 611695064,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRequestSizes/Payload-10000KB - allocs/op",
+            "value": 247,
             "unit": "allocs/op",
             "extra": "1 times\n4 procs"
           }
