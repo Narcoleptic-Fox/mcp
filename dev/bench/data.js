b8044bb1cd8c323d63c7b09e8d51d465716cab0c
github-action-benchmark <github@users.noreply.github.com>
2025-05-16 04:19:58 +0000

    add Benchmark (go) benchmark result for ca549a94740253196188e4ff25018eb6fef435e9

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1747369198267,
+  "repoUrl": "https://github.com/Narcoleptic-Fox/mcp",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "bwclaus@gmail.com",
+            "name": "Brock Claussen",
+            "username": "Dieshen"
+          },
+          "committer": {
+            "email": "bwclaus@gmail.com",
+            "name": "Brock Claussen",
+            "username": "Dieshen"
+          },
+          "distinct": true,
+          "id": "ca549a94740253196188e4ff25018eb6fef435e9",
+          "message": "updated for workflow permissions",
+          "timestamp": "2025-05-16T00:19:00-04:00",
+          "tree_id": "1b343ee79eb7b004eda58374a6367a179f4c9159",
+          "url": "https://github.com/Narcoleptic-Fox/mcp/commit/ca549a94740253196188e4ff25018eb6fef435e9"
+        },
+        "date": 1747369197442,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkRequestSizes/Payload-10000KB",
+            "value": 2437124526,
+            "unit": "ns/op\t628472912 B/op\t     274 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRequestSizes/Payload-10000KB - ns/op",
+            "value": 2437124526,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRequestSizes/Payload-10000KB - B/op",
+            "value": 628472912,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRequestSizes/Payload-10000KB - allocs/op",
+            "value": 274,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
